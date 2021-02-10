88052b0f21a7ee5f23bafa594171b6ae55faa3d1
Sunny Kwong <sunnykwong91500@gmail.com>
2021-02-10 17:46:32 -0500

    major updates, simplebar, css styling, added information about commands to docs

--- src/components/DocServerManagement.js
+++ src/components/DocServerManagement.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import DocSidebar from '../components/DocSidebar.js';
 import docStyles from '../stylesheets/docs.module.css';
+import SimpleBar from 'simplebar-react';
+import 'simplebar/dist/simplebar.min.css';
 
 const DocServerManagement = ({sections, section, setSection}) => {
 
@@ -9,7 +11,7 @@ const DocServerManagement = ({sections, section, setSection}) => {
         <div>
             <DocSidebar sections = {sections} currentSection = {section} setSection = {setSection}/>
         </div>
-        <div className = {docStyles.documentation}>
+        <SimpleBar className = {docStyles.documentation}>
             <div>
                 <h2 className = {docStyles.sectionHeading}>Server Management</h2>
                 Manage your server with our suite of management commands!
@@ -33,8 +35,8 @@ const DocServerManagement = ({sections, section, setSection}) => {
                     </div>
                     <br />
                     <div className = {docStyles.commandUsage}>
-                        <span className = {docStyles.comment}># include the -n flag if you want to notify the user of their ban </span><br />
-                        <span className = {docStyles.comment}># include the -m flag if you want to include a reason for the ban </span><br /><br />
+                        <span className = {docStyles.comment}># [optional] include the -n flag if you want to notify the user of their ban </span><br />
+                        <span className = {docStyles.comment}># [optional] include the -m flag if you want to include a reason for the ban </span><br /><br />
                         ~ban @user ( -n / -m "reason" )<br />
                     </div>
                     <br />
@@ -76,8 +78,8 @@ const DocServerManagement = ({sections, section, setSection}) => {
                     </div>
                     <br />
                     <div className = {docStyles.commandUsage}>
-                        <span className = {docStyles.comment}># include the -n flag if you want to notify the user of their kick </span><br />
-                        <span className = {docStyles.comment}># include the -m flag if you want to include a reason for the kick </span><br /><br />
+                        <span className = {docStyles.comment}># [optional] include the -n flag if you want to notify the user of their kick </span><br />
+                        <span className = {docStyles.comment}># [optional] include the -m flag if you want to include a reason for the kick </span><br /><br />
                         ~kick @user ( -n / -m "reason" )<br />
                     </div>
                     <br />
@@ -100,13 +102,105 @@ const DocServerManagement = ({sections, section, setSection}) => {
 
                 <br />
 
+                {/*SETMAXWARNINGS COMMAND*/}
+                <div className = {docStyles.subsection}>
+                    <h3 className = {docStyles.commandHeading}>Setmaxwarnings</h3>
+                    You can set the server's maximum amount of warnings a user can attain before they get banned. <br /><br />
+                    <div>
+                        To use this command just use the following format:
+                    </div><br />
+
+                    <div className = {docStyles.commandUsage}>
+                        <span className = {docStyles.comment}># sets the server's max amount of warnings before a user gets banned to n amount</span><br /><br />
+                        ~setmaxwarnings n
+                    </div><br />
+
+                    <div>
+                        Examples:
+                    </div><br />
+
+                    <div className = {docStyles.commandUsage}>
+                        <span className = {docStyles.comment}># sets the server's number of max warnings to 5</span><br /><br />
+                        ~setmaxwarnings 5
+                    </div><br />
+
+                    <div>
+                        Permissions: ADMINISTRATOR
+                    </div>
 
 
+                </div>
+                {/*END SETMAXWARNINGS*/}
 
+                <br />
+
+                
+                {/*UNBAN COMMAND*/}
+                <div className = {docStyles.subsection}>
+                    <h3 className = {docStyles.commandHeading}>Unban</h3>
+                    Unbans a user from the server. <br /><br />
+                    <div>
+                        To use this command just use the following format:
+                    </div><br />
+
+                    <div className = {docStyles.commandUsage}>
+                        <span className = {docStyles.comment}># userId is the Discord ID given to the user</span><br /><br />
+                        ~unban userId
+                    </div><br />
+
+                    <div>
+                        Examples:
+                    </div><br />
+
+                    <div className = {docStyles.commandUsage}>
+                        <span className = {docStyles.comment}># unbans the users with id: 00000000000000000001</span><br /><br />
+                        ~unban 00000000000000000001
+                    </div><br />
+
+                    <div>
+                        Permissions: BAN_MEMBERS
+                    </div>
+
+
+                </div>
+                {/*END SETMAXWARNINGS*/}
+
+                <br />
+
+                {/*WARN COMMAND SUBSECTION*/}
+                <div className = {docStyles.subsection}>
+                    <h3 className = {docStyles.commandHeading}>Warn</h3>
+                        Warns a user in the server, if the user exceeds the max amount of warnings (default: 2) they will get banned.<br /><br />
+                    <div>
+                        To use this command just follow the following format:
+                    </div>
+                    <br />
+                    <div className = {docStyles.commandUsage}>
+                        <span className = {docStyles.comment}># @user is a Discord mention to that user </span><br />
+                        <span className = {docStyles.comment}># [optional] warning is the reason you are warning the user for </span><br /><br />
+                        ~warn @user "warning"
+                    </div>
+                    <br />
+                    <div>
+                        Examples:
+                    </div>
+                    <br />
+                    <div className = {docStyles.commandUsage}>
+                        <span className = {docStyles.comment}># warn TOA5T for spamming chat</span><br /><br />
+                        ~warn @TOA5T "spamming chat"<br /><br />
+                        <span className = {docStyles.comment}># warn TOA5T</span><br /><br />
+                        ~warn @TOA5T
+                    </div>
+                    <br />
+                    <div>
+                        Permissions: BAN_MEMBERS
+                    </div>
+                </div>
+                {/*END KICK SUBSECTION*/}
 
 
             </div>
-        </div>
+        </SimpleBar>
     </div>
     );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,32 @@
 import * as React from "react"
 import Header from '../components/Header.js';
 import styles from '../stylesheets/index.module.css';
+import discordTransparent from '../images/discord-transparent.png';
 
 // markup
 const IndexPage = () => {
   return (
     <main style={styles}>
       <Header/>
-    
-      <a href = "https://discord.com/oauth2/authorize?client_id=795736084327301160&scope=bot&permissions=8" className = {styles.discordButton}>Discord</a>
-    
+      <div>
+        <a href = "https://discord.com/oauth2/authorize?client_id=795736084327301160&scope=bot&permissions=8" className = {styles.discordButton}>
+          <img className = {styles.discordLogo} src = {discordTransparent}/> Add to Discord
+        </a>
+
+        <div className = {styles.footer}>
+          <div>
+            Easy to use commands!
+          </div>
+          <div>
+            Manage your server with ease!
+          </div>
+          <div>
+            Music playability!
+          </div>
+        </div>
+
+
+      </div>
     </main>
   )
 }
